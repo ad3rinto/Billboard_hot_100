b371e42a9832697ca61fbe924532edfff731e961
ad3rinto <aad3rinto@gmail.com>
2024-05-22 17:44:52 +0100

    fixed it

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ content = web_data.text
 soup = BeautifulSoup(content, "html.parser")
 # print(soup.prettify())
 
-title = soup.findAll("h3", class_="c-title")
-for i in title:
-    print(i.text)
+song_name_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_name_spans]
+# print(song_names)
+
+song_artist_spans = soup.select("li ul li span")
+artist_names = [artist.getText().strip() for artist in song_artist_spans]
+# print(artist_names)
+new_artist_names = [item for item in artist_names if not item.isdigit()]
+# print(new_artist_names)
+
+for (title, artist) in zip(song_names, new_artist_names):
+    print(f"Song is: {title}\nand artist is {artist}")
+
