bd4ef1bbf3bd96eaf422b354d9cb79bddfcaf2db
ad3rinto <aad3rinto@gmail.com>
2024-05-22 15:11:35 +0100

    wip

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-year_of_travel = input("What year will you like to travel to ? YYY-MM-DD format please: ")
+import requests
+from bs4 import BeautifulSoup
 
-print(year_of_travel)
+year_of_travel = input("What year will you like to travel to ? YYY-MM-DD format please: ")
+# print(year_of_travel)
+
+URL = f"https://www.billboard.com/charts/hot-100/{year_of_travel}/"
+
+web_data = requests.get(URL)
+content = web_data.text
+
+soup = BeautifulSoup(content, "html.parser")
+# print(soup.prettify())
+
+title = soup.findAll("h3", class_="c-title")
+for i in title:
+    print(i.text)
