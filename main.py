2409d83868bc2479cd6d2f708a2f049579cc8e4c
ad3rinto <aad3rinto@gmail.com>
2024-05-24 11:20:21 +0100

    m

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
 
 year_of_travel = input("What year will you like to travel to ? YYY-MM-DD format please: ")
 # print(year_of_travel)
