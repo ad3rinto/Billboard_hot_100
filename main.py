2b3770f644e112c1344b2b773ff9cf6b573299d2
ad3rinto <aad3rinto@gmail.com>
2024-05-24 11:48:50 +0100

    not working here

--- main.py
+++ main.py
@@ -2,30 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 
-year_of_travel = input("What year will you like to travel to ? YYY-MM-DD format please: ")
-# print(year_of_travel)
+SPOTIPY_CLIENT_ID =os.environ.get['CLIENT_ID']
+SPOTIPY_CLIENT_SECRET=os.environ.get['CLIENT_SECRET']
 
-URL = f"https://www.billboard.com/charts/hot-100/{year_of_travel}/"
+print(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
 
-web_data = requests.get(URL)
-content = web_data.text
+# year_of_travel = input("What year will you like to travel to ? YYY-MM-DD format please: ")
+# # print(year_of_travel)
 
-soup = BeautifulSoup(content, "html.parser")
-# print(soup.prettify())
+# URL = f"https://www.billboard.com/charts/hot-100/{year_of_travel}/"
 
-song_name_spans = soup.select("li ul li h3")
-song_names = [song.getText().strip() for song in song_name_spans]
-# print(song_names)
+# web_data = requests.get(URL)
+# content = web_data.text
 
-song_artist_spans = soup.select("li ul li span")
-artist_names = [artist.getText().strip() for artist in song_artist_spans]
-# print(artist_names)
-new_artist_names = [item for item in artist_names if not item.isdigit()]
-# print(new_artist_names)
-pos = 1
-for (title, artist) in zip(song_names, new_artist_names):
-    print(f"At position {pos} is: {title}\nArtist: {artist}\n")
-    pos += 1
+# soup = BeautifulSoup(content, "html.parser")
+# # print(soup.prettify())
+
+# song_name_spans = soup.select("li ul li h3")
+# song_names = [song.getText().strip() for song in song_name_spans]
+# # print(song_names)
+
+# song_artist_spans = soup.select("li ul li span")
+# artist_names = [artist.getText().strip() for artist in song_artist_spans]
+# # print(artist_names)
+# new_artist_names = [item for item in artist_names if not item.isdigit()]
+# # print(new_artist_names)
+# pos = 1
+# for (title, artist) in zip(song_names, new_artist_names):
+#     print(f"At position {pos} is: {title}\nArtist: {artist}\n")
+#     pos += 1
