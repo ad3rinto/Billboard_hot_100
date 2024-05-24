ceb33db17cb7fe174baf6cf41d9f65af58f1f445
Micheal A <aad3rinto@gmail.com>
2024-05-24 12:19:46 +0000

    get boilerplate spotipy code to run thus validating OAUTH

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+# SPOTIPY_CLIENT_ID =os.environ.get('CLIENT_ID')
+# SPOTIPY_CLIENT_SECRET=os.environ.get('CLIENT_SECRET')
+# print(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
 
-SPOTIPY_CLIENT_ID =os.environ.get['CLIENT_ID']
-SPOTIPY_CLIENT_SECRET=os.environ.get['CLIENT_SECRET']
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-print(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
 
+for album in albums:
+    print(album['name'])
+    
 # year_of_travel = input("What year will you like to travel to ? YYY-MM-DD format please: ")
 # # print(year_of_travel)
 
